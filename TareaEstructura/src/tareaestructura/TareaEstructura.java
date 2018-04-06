ac6d1ea1a14610edb3c528b9d7f31805c3ada945
magneto19 <38114590+magneto19@users.noreply.github.com>
2018-04-05 21:22:30 -0500

    Update TareaEstructura.java

--- TareaEstructura/src/tareaestructura/TareaEstructura.java
+++ TareaEstructura/src/tareaestructura/TareaEstructura.java
@@ -5,49 +5,52 @@ import javax.swing.JOptionPane;
 /**
  * @author Johan Sebastian Lasso Rivas
  */
+
 public class TareaEstructura{
 
     public static void main(String[] args) {
+  
+        /* A CONTINUACION LE HAGO UNA PEQUEÑA MODIFICACION AL PROYETO PARA VER SUS RESPECTIVOS CAMBIOS*/
         
         int x=0;
         int opcion;
         
         do{
+            
            opcion = Integer.parseInt(JOptionPane.showInputDialog("Digite su opcion: \n 1-PAISES \n 2-CAPITALES \n 3-CONTINENTE \n 4-SALIR"));
            String paisesMatriz[][] = {{"Afganistan","Colombia","Alemania","Camerun","Canada"},{"Kabul","Bogotá","Berlín","Yaundé","Ottawa"},{"Asia","América","Europa","África","Ameríca"}};
            switch(opcion){
+               
                case 1:
                    for(int i=0;i<=4;i++){
-                     
-                    JOptionPane.showMessageDialog(null, "Pais "+paisesMatriz[0][i]);
+                   
+                    JOptionPane.showMessageDialog(null, "PAIS "+paisesMatriz[0][i]);
                    }
                 break;
                 
                 case 2:
                     for(int i=0;i<=4;i++){
                      
-                    JOptionPane.showMessageDialog(null, "Capital "+paisesMatriz[1][i]);
+                    JOptionPane.showMessageDialog(null, "CAPITAL "+paisesMatriz[1][i]);
                    } 
                 break;
                 
                 case 3:
                     for(int i=0;i<=4;i++){
                      
-                    JOptionPane.showMessageDialog(null, "Continente "+paisesMatriz[2][i]);
+                    JOptionPane.showMessageDialog(null, "CONTINENTE "+paisesMatriz[2][i]);
                    }                   
                 break;
                 
                 case 4:             
-                    JOptionPane.showMessageDialog(null, "Ha seleccionado salir");
+                    JOptionPane.showMessageDialog(null, "PROCESO TERMINADO");
                     x=1;
                 break;
                 
                 default:
-                    JOptionPane.showMessageDialog(null, "Esta opccion no existe"); 
+                    JOptionPane.showMessageDialog(null, "Esta opccion no existe... DIGITE CORRECTAMENTE LOS DATOS"); 
                
            }
-
-        }while(x==0);
-    }
-    
-}
+         } while(x==0);
+       }
+     }
